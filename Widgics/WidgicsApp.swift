907f5af4201f745ab8e593159d14633797487e1d
Pushpinder Pal Singh <pushpinderpal19@gmail.com>
2021-05-01 19:06:04 +0530

    Initial Commit

---
+++ Widgics/WidgicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgicsApp.swift
+//  Widgics
+//
+//  Created by Pushpinder Pal Singh on 01/05/21.
+//
+
+import SwiftUI
+
+@main
+struct WidgicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
