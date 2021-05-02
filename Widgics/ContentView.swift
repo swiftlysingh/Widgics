7134f6b35d3fb6848399ef0ebec1f4feb7858635
Pushpinder Pal Singh <pushpinderpal19@gmail.com>
2021-05-02 07:25:52 +0530

    Add Home Page

--- Widgics/ContentView.swift
+++ Widgics/ContentView.swift
@@ -9,7 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+		TabView {
+			HomeView()
+				.tabItem {
+					Image(systemName: "house.fill")
+					Text("Home")
+				}
+			SettingsView(viewModel: .init())
+				.tabItem {
+					Image(systemName: "gearshape.fill")
+					Text("Settingss")
+				}
+		}
     }
 }
 
---
+++ Widgics/HomeView.swift
@@ -0,0 +1,46 @@
+//
+//  HomeView.swift
+//  Widgics
+//
+//  Created by Pushpinder Pal Singh on 02/05/21.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+	
+	@State private var isAlertShown = false
+	
+	private var addButton: some View {
+		AnyView(Button(action: { isAlertShown = true }) {
+			Image(systemName: "plus")
+		})
+	}
+	
+	var body: some View {
+		NavigationView {
+			List {
+				Text("Hello")
+			}
+			.navigationBarTitle("Home")
+			.navigationViewStyle(StackNavigationViewStyle())
+			.navigationBarItems(trailing: addButton)
+		}
+		.alert(isPresented: $isAlertShown, TextAlert(title: "Add Sites", placeholder: "Enter a URL for a site", accept: "Add", cancel: "Cancel", action: onAdd ))
+	}
+}
+
+struct HomeView_Previews: PreviewProvider {
+	static var previews: some View {
+		HomeView()
+	}
+}
+
+//MARK: - Private
+
+extension HomeView {
+	private func onAdd(username: String?){
+		guard let username = username else { return }
+		// Add site into Singleton.
+	}
+}
