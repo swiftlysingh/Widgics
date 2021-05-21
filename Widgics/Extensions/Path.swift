c8592ef5b670aedc9193edadb8cbd9c8d5c8ad0b
Pushpinder Pal Singh <pushpinderpal19@gmail.com>
2021-05-22 05:09:22 +0530

    Refactor Graph

--- Widgics/Extensions/Path.swift
+++ Widgics/Extensions/Path.swift
@@ -7,18 +7,53 @@
 
 import SwiftUI
 
-extension Path {
+struct Graph: Shape {
 
-	static func lineChart(points:[Int], step:CGPoint) -> Path {
+	var data : [Int]
+	@Binding var frame : CGRect
+	var padding : CGFloat
+
+	init(data : [Int], frame : Binding<CGRect>, padding:CGFloat = 30) {
+		self.data = data
+		self._frame = frame
+		self.padding = padding
+	}
+	var stepWidth: CGFloat {
+		if data.count < 2 {
+			return 0
+		}
+		return frame.size.width / CGFloat(data.count-1)
+	}
+	var stepHeight: CGFloat {
+		var min: Int?
+		var max: Int?
+		if let minPoint = data.min(), let maxPoint = data.max(), minPoint != maxPoint {
+			min = minPoint
+			max = maxPoint
+		}else {
+			return 0
+		}
+		if let min = min, let max = max, min != max {
+			if (min <= 0){
+				return (frame.size.height-padding) / CGFloat(max - min)
+			}else{
+				return (frame.size.height-padding) / CGFloat(max + min)
+			}
+		}
+
+		return 0
+	}
+
+	func path(in rect: CGRect) -> Path {
 		var path = Path()
-		if (points.count < 2){
+		if (data.count < 2){
 			return path
 		}
-		guard let offset = points.min() else { return path }
-		let p1 = CGPoint(x: 0, y: CGFloat(points[0]-offset)*step.y)
+		guard let offset = data.min() else { return path }
+		let p1 = CGPoint(x: 0, y: CGFloat(data[0]-offset)*stepHeight)
 		path.move(to: p1)
-		for pointIndex in 1..<points.count {
-			let p2 = CGPoint(x: step.x * CGFloat(pointIndex), y: step.y*CGFloat(points[pointIndex]-offset))
+		for pointIndex in 1..<data.count {
+			let p2 = CGPoint(x: stepWidth * CGFloat(pointIndex), y: stepHeight*CGFloat(data[pointIndex]-offset))
 			path.addLine(to: p2)
 		}
 		return path
